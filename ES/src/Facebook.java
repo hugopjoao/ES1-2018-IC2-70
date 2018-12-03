a3df72316a0649457a0fbd3b499a20102fc1f65e
filip <filip@192.168.1.70>
2018-12-03 18:28:23 +0000

    Update da classe facebook

--- ES/src/Facebook.java
+++ ES/src/Facebook.java
@@ -3,9 +3,18 @@ import com.restfb.types.Thread;
 public class Facebook extends Thread {
 
 	private Interface_Grafica gui;
+	private String accessToken;
 	
-	public Facebook (Interface_Grafica gui) {
+	
+	/* Access token:
+	 EAAMZCl2Ln2ZAkBABIv1qIFGFXd93JrHkt9Qpwhlsx2MZA4UcIw2OJMOtqRSGMqluWdqvdgwfNEeTdnnb82tcqeh7Ns3WlKF1aHZBNEmoJv9zhfIdG97hTROZBhVGgnAl5z3JnvHZAjy14m79Iu9CYqwlOwktBAd6j394w5ISnJdtpSiqQZCfKWbYn1YjvAjZBNzWXeEoDFmMNYrzMyUQFxgWxlSPZCLth204qhKyrzVfjygZDZD */
+	
+	public Facebook (String accessToken, Interface_Grafica gui) {
+		this.accessToken = accessToken;
 		this.gui = gui;
 	}
+	
+	
+	
 	
 }
